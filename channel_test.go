1e8082a2068da3074d7a18b6ac26bbc74dcedfc3
csystem-it <csystem-it@users.noreply.github.com>
2021-06-13 21:53:25 +0200

    Update channel_test.go

--- channel_test.go
+++ channel_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/csystem-it/go2node/ipc""
+	"github.com/csystem-it/go2node/ipc"
 )
 
 const testFile = "testdata/channel_test.js"
